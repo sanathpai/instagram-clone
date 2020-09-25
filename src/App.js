e6ecfa11dae39c334e8919e5925bfff0c7350a5c
sanathpai <sanathrpai99@gmail.com>
2020-09-25 20:49:46 +0530

    props works!

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ function App() {
       <div className="app__header">
         <img className="app__headerImage" alt="" src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png" />
       </div>
-      <Post />
+      <Post username="sanathpai99" caption="what a frikkin idiot!" imageUrl="https://images.unsplash.com/photo-1596650956793-68f12df4e549?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" />
+      <Post username="anushamlvalli" caption="hahaha!" imageUrl="https://images.unsplash.com/photo-1600115010857-edbbc7e1efb8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" />
     </div>
   );
 }
--- src/Post.js
+++ src/Post.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import './Post.css';
 import Avatar from "@material-ui/core/Avatar";
-function Post() {
+function Post({ username, caption, imageUrl }) {
     return (
         <div className="post">
             <div className="post__header">
-                <Avatar className="post__avatar" alt="Sanathpai99" src="/static/images/avatar/1.jpg" />
-                <h3>Username</h3>
+                <Avatar className="post__avatar" alt={username} src="/static/images/avatar/1.jpg" />
+                <h3>{username}</h3>
             </div>
-            <img className="post__image" src="https://images.unsplash.com/photo-1558900243-1e6ef4670dc3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" alt=" " />
-            <h4 className="post__text"><strong>Sanathpai99:</strong> what a frikkin idiot!</h4>
+            <img className="post__image" src={imageUrl} alt=" " />
+            <h4 className="post__text"><strong>{username}</strong> {caption}</h4>
         </div>
     )
 }
