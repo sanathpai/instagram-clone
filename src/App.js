1da1e1af0472f738f61c82d35dc3510208a9a1d3
sanathpai <sanathrpai99@gmail.com>
2020-09-27 15:09:36 +0530

    useEffect used for posts

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import Post from './Post';
-
+import { db } from './firebase';
 function App() {
   const [posts, setPosts] = useState([
-    {
-      username: "sanathpai99",
-      caption: "what a frikkin idiot!",
-      imageUrl: "https://images.unsplash.com/photo-1596650956793-68f12df4e549?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
-    },
-    {
-      username: "anushamlvalli",
-      caption: "hahaha!",
-      imageUrl: "https://images.unsplash.com/photo-1600115010857-edbbc7e1efb8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
-    }
+    // {
+    //   username: "sanathpai99",
+    //   caption: "what a frikkin idiot!",
+    //   imageUrl: "https://images.unsplash.com/photo-1596650956793-68f12df4e549?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
+    // },
+    // {
+    //   username: "anushamlvalli",
+    //   caption: "hahaha!",
+    //   imageUrl: "https://images.unsplash.com/photo-1600115010857-edbbc7e1efb8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
+    // }
   ]);
+  useEffect(() => {
+    db.collection('posts').onSnapshot(snapshot => {
+      setPosts(snapshot.docs.map(doc => doc.data()))
+    });
+  }, [])
   return (
     <div className="app">
       <div className="app__header">
